90e7177a19432bcc166bf7d2f230bead5ce91e73
ketan0608 <76109939+ketan0608@users.noreply.github.com>
2021-07-09 22:01:15 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    text("WRITE IN CAPITAL LETTERS!!",100,90)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+fill("white")
+    textSize(15)
+    text("R E V B A I L A", 700,150)
+    text("WRITE IN CAPITAL LETTERS!!",700,190)
+    fill("lightblue")
+    text("Hint: CUTNIFON !!", 100,70)
+
+    fill("white")
+        textSize(15)
+        text("R E V B A I L A", 300,250)
+        text("WRITE IN CAPITAL LETTERS!!",300,290)
+        fill("lightblue")
+        text("Hint: ATADASBA !!", 100,70)
+    }
